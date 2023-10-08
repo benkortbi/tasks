0f5c12e06ea218096534a039a49103dd524e80f9
benkortbi <benkortbi@users.noreply.github.com>
2023-10-08 18:52:52 +0000

    deploy: f702a8840bd40a5ed3935e8bf981379ce16fc7f7

---
+++ assets/NotFount-7683c2ee.js
@@ -0,0 +1,1 @@
+import{j as t}from"./index-09d378ac.js";function n(){return t.jsx("div",{className:"w-full h-screen grid place-content-center text-white",children:"404 | NOT FOUND"})}export{n as default};
---
+++ assets/formatteDate-c212038f.js
@@ -0,0 +1,1 @@
+function r(e){return new Date(e).toLocaleString("en-GB",{day:"numeric",month:"numeric",year:"numeric",hour:"numeric",minute:"numeric"})}export{r as f};
